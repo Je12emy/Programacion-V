9844bb0017ae370573c85eb2acbec31296781e41
Jeremy <jeremyzelaya@hotmail.es>
2019-05-27 21:01:04 -0600

    Agregando MVC Party

---
+++ Segunda Semana/MVC_Party/MVC_Party/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Party.Models;
+
+namespace MVC_Party.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Reservar()
+        {
+            //Guests g = new Models.Guests();
+            //g.Nombre = "";
+            //g.Email = "";
+
+            return View();
+        }
+    }
+}
---
+++ Segunda Semana/MVC_Party/MVC_Party/Models/Guests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Party.Models
+{
+    public class Guests
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public bool? Asistencia { get; set; } //A esta variable se le puede asignar TRUE o FALSE como cualquier Boolean pero puede tener el valor NULL
+    }
+}
