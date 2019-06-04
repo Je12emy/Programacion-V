6d4b80483b442c8b33fb8e1ed947f38fcf7dcf7b
Jeremy <jeremyzelaya@hotmail.es>
2019-06-03 20:51:51 -0600

    Added MVC_Ventas
    Programa de practica de la clase

---
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Models/Workers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Ventas.Models
+{
+    public class Workers
+    {
+        //Arrays con los datos de la planilla.
+        public int [] code { get; set; }
+        public string [] role { get; set; }
+        public string [] name { get; set; }
+        public int [] salary { get; set; }
+
+    }
+}
