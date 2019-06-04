262c972e8b4d0518714e074a15ab2b669a828839
Jeremy <jeremyzelaya@hotmail.es>
2019-06-03 20:19:14 -0600

    Added finished MVC_Party
    Tuve que borrar el MVC_Part original pero esta es la version actualizada.

--- Segunda Semana/MVC_Party/MVC_Party/Controllers/HomeController.cs
+++ Segunda Semana/MVC_Party/MVC_Party/Controllers/HomeController.cs
@@ -14,6 +14,8 @@ namespace MVC_Party.Controllers
         {
             return View();
         }
+        [HttpGet] //Se usa para visualizar la forma
+        //La primera vez que se carga, se hace el GET
         public ActionResult Reservar()
         {
             //Guests g = new Models.Guests();
@@ -22,5 +24,19 @@ namespace MVC_Party.Controllers
 
             return View();
         }
+        //Controlador para la reserva
+        [HttpPost] //Capturar los datos de la forma y enviarlos
+        //Al hacer un submit, se da el POST y se ejecuta la clase
+        public ViewResult Reservar(Guests gr) {
+            if (ModelState.IsValid) //ModelState.IsValid, se va a modelo de Datos y ahi hace las validaciones correspondientes.
+            {
+                return View("Gracias", gr);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
--- Segunda Semana/MVC_Party/MVC_Party/Models/Guests.cs
+++ Segunda Semana/MVC_Party/MVC_Party/Models/Guests.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations; //Permite agregar validaciones al modelo
 
 namespace MVC_Party.Models
 {
     public class Guests
     {
+        [Required(ErrorMessage = "Por favor ingrese su nombre.")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Por favor ingrese su correo.")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Por favor ingrese un correo valido.")] 
         public string Email { get; set; }
         public string Celular { get; set; }
         public bool? Asistencia { get; set; } //A esta variable se le puede asignar TRUE o FALSE como cualquier Boolean pero puede tener el valor NULL
+
     }
 }
