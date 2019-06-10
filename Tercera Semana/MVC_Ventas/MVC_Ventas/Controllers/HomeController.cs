6c1ad5908bff782adab39066d6f716999201daa7
Jeremy <jeremyzelaya@hotmail.es>
2019-06-09 19:41:06 -0600

    Update on MVC_Ventas
    Added basic display for data, I also tried to pass data from the Model between views but I was unable to make it work.

--- Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
@@ -3,15 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Ventas.Models;
 
 namespace MVC_Ventas.Controllers
 {
     public class HomeController : Controller
     {
+        
+        //public void Cargar() {
+        //    //Carga de Datos predeterminados
+           
+
+        //    //Definir los codigos de los empleados.
+        //    w.code[0] = 01;
+        //    w.code[1] = 02;
+        //    w.code[2] = 03;
+        //    w.code[3] = 04;
+
+        //    //Definir los nombres de los empleados.
+        //    w.name[0] = "Jeremy Zelaya";
+        //    w.name[1] = "Susan Solano";
+        //    w.name[2] = "Tamara Richards";
+        //    w.name[3] = "Julian Rodriguez";
+
+        //    //Definir los roles de los empleados.
+        //    w.role[0] = "IT";
+        //    w.role[1] = "Proyect Manager";
+        //    w.role[2] = "Equipment Maintenance";
+        //    w.role[3] = "Production Supervisor";
+
+        //    //Definir los salarios de los empleados.
+        //    w.salary[0] = 1500000;
+        //    w.salary[1] = 2000000;
+        //    w.salary[2] = 1200000;
+        //    w.salary[3] = 1500000;
+
+        //}
+
+        //Workers w = new Models.Workers();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            //Como al inicar el programa, este metodo se iniciara para mostra la vista aca cargare los datos por primera vez.
+            //Cargar();
+            //Carga de Datos predeterminados
+            Workers w = new Models.Workers();
+
+            //Definir los codigos de los empleados.
+            w.code[0] = 01;
+            w.code[1] = 02;
+            w.code[2] = 03;
+            w.code[3] = 04;
+
+            //Definir los nombres de los empleados.
+            w.name[0] = "Jeremy Zelaya";
+            w.name[1] = "Susan Solano";
+            w.name[2] = "Tamara Richards";
+            w.name[3] = "Julian Rodriguez";
+
+            //Definir los roles de los empleados.
+            w.role[0] = "IT";
+            w.role[1] = "Proyect Manager";
+            w.role[2] = "Equipment Maintenance";
+            w.role[3] = "Production Supervisor";
+
+            //Definir los salarios de los empleados.
+            w.salary[0] = 1500000;
+            w.salary[1] = 2000000;
+            w.salary[2] = 1200000;
+            w.salary[3] = 1500000;
+
+            return View(w);
         }
+        public ActionResult Configuracion(Workers w)    //Recibir una instancia de Workers como parametro y pasarlo al view.
+        {
+            //Averiguar como pasar los datos cargados desde Index a esta clase.
+            //Workers w = new Models.Workers();
+            //w.code[0] = 01;
+            //w.code[1] = 02;
+            //w.code[2] = 03;
+            //w.code[3] = 04;
+            //w.code[0] = 01;
+
+            return View(w);
+        }       
     }
 }
--- Tercera Semana/MVC_Ventas/MVC_Ventas/Models/Workers.cs
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Models/Workers.cs
@@ -8,10 +8,11 @@ namespace MVC_Ventas.Models
     public class Workers
     {
         //Arrays con los datos de la planilla.
-        public int [] code { get; set; }
-        public string [] role { get; set; }
-        public string [] name { get; set; }
-        public int [] salary { get; set; }
+        //Cada arreglo es de 4 items.
+        public int [] code { get; set; } = new int[4];
+        public string [] role { get; set; } = new string [4];
+        public string [] name { get; set; } = new string[4];
+        public int [] salary { get; set; } = new int [4];
 
     }
 }
