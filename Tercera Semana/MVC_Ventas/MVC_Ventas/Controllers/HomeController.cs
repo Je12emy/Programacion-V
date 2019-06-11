1cb538c2e12e6a09ddb5b8d3fcb932b296fc9d30
Jeremy @UH <JeremyZelaya@hotmail.com>
2019-06-10 21:05:01 -0600

    added WS

--- Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MVC_Ventas.Controllers
     public class HomeController : Controller
     {
         
-        //public void Cargar() {
+        // public void Cargar() {
         //    //Carga de Datos predeterminados
            
 
@@ -40,35 +40,35 @@ namespace MVC_Ventas.Controllers
 
         //}
 
-        //Workers w = new Models.Workers();
+        // Workers w = new Models.Workers();
 
         // GET: Home
         public ActionResult Index()
         {
-            //Como al inicar el programa, este metodo se iniciara para mostra la vista aca cargare los datos por primera vez.
-            //Cargar();
-            //Carga de Datos predeterminados
+            // Como al inicar el programa, este metodo se iniciara para mostra la vista aca cargare los datos por primera vez.
+            // Cargar();
+            // Carga de Datos predeterminados.
             Workers w = new Models.Workers();
 
-            //Definir los codigos de los empleados.
+            // Definir los codigos de los empleados.
             w.code[0] = 01;
             w.code[1] = 02;
             w.code[2] = 03;
             w.code[3] = 04;
 
-            //Definir los nombres de los empleados.
+            // Definir los nombres de los empleados.
             w.name[0] = "Jeremy Zelaya";
             w.name[1] = "Susan Solano";
             w.name[2] = "Tamara Richards";
             w.name[3] = "Julian Rodriguez";
 
-            //Definir los roles de los empleados.
+            // Definir los roles de los empleados.
             w.role[0] = "IT";
             w.role[1] = "Proyect Manager";
             w.role[2] = "Equipment Maintenance";
             w.role[3] = "Production Supervisor";
 
-            //Definir los salarios de los empleados.
+            // Definir los salarios de los empleados.
             w.salary[0] = 1500000;
             w.salary[1] = 2000000;
             w.salary[2] = 1200000;
