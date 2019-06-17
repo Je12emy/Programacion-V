5e626fd43da9d61dfb1e182435a2e6190e1ae559
Jeremy <jeremyzelaya@hotmail.es>
2019-06-17 15:41:33 -0600

    Finished MVC_Ventas

--- Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Controllers/HomeController.cs
@@ -9,44 +9,21 @@ namespace MVC_Ventas.Controllers
 {
     public class HomeController : Controller
     {
-        
-        // public void Cargar() {
-        //    //Carga de Datos predeterminados
-           
 
-        //    //Definir los codigos de los empleados.
-        //    w.code[0] = 01;
-        //    w.code[1] = 02;
-        //    w.code[2] = 03;
-        //    w.code[3] = 04;
 
-        //    //Definir los nombres de los empleados.
-        //    w.name[0] = "Jeremy Zelaya";
-        //    w.name[1] = "Susan Solano";
-        //    w.name[2] = "Tamara Richards";
-        //    w.name[3] = "Julian Rodriguez";
-
-        //    //Definir los roles de los empleados.
-        //    w.role[0] = "IT";
-        //    w.role[1] = "Proyect Manager";
-        //    w.role[2] = "Equipment Maintenance";
-        //    w.role[3] = "Production Supervisor";
-
-        //    //Definir los salarios de los empleados.
-        //    w.salary[0] = 1500000;
-        //    w.salary[1] = 2000000;
-        //    w.salary[2] = 1200000;
-        //    w.salary[3] = 1500000;
-
-        //}
-
-        // Workers w = new Models.Workers();
 
         // GET: Home
+
+        // Vista para entrar a la plataforma.
         public ActionResult Index()
         {
-            // Como al inicar el programa, este metodo se iniciara para mostra la vista aca cargare los datos por primera vez.
-            // Cargar();
+            return View();
+        }
+
+        // Pagina para la vista de la tabla.
+        [HttpGet]
+        public ActionResult tabla()
+        {
             // Carga de Datos predeterminados.
             Workers w = new Models.Workers();
 
@@ -55,27 +32,42 @@ namespace MVC_Ventas.Controllers
             w.code[1] = 02;
             w.code[2] = 03;
             w.code[3] = 04;
+            w.code[4] = 05;
 
             // Definir los nombres de los empleados.
             w.name[0] = "Jeremy Zelaya";
             w.name[1] = "Susan Solano";
             w.name[2] = "Tamara Richards";
             w.name[3] = "Julian Rodriguez";
+            w.name[4] = "Angeles Rostran";
 
             // Definir los roles de los empleados.
             w.role[0] = "IT";
             w.role[1] = "Proyect Manager";
             w.role[2] = "Equipment Maintenance";
             w.role[3] = "Production Supervisor";
+            w.role[4] = "Accountant";
 
             // Definir los salarios de los empleados.
             w.salary[0] = 1500000;
             w.salary[1] = 2000000;
             w.salary[2] = 1200000;
             w.salary[3] = 1500000;
-
+            w.salary[4] = 1750000;
             return View(w);
         }
+        [HttpPost]
+        public ActionResult tabla(Workers w) {
+            if (ModelState.IsValid)
+            {
+                // Retorna el View 'Datos' con la instancia del modelo de 'Workers'.
+                return View("Datos",w);
+            }
+            else           
+                // Retorna al View de Tabla.
+                return View();
+        }
+
         public ActionResult Configuracion(Workers w)    //Recibir una instancia de Workers como parametro y pasarlo al view.
         {
             //Averiguar como pasar los datos cargados desde Index a esta clase.
--- Tercera Semana/MVC_Ventas/MVC_Ventas/Models/Workers.cs
+++ Tercera Semana/MVC_Ventas/MVC_Ventas/Models/Workers.cs
@@ -9,10 +9,47 @@ namespace MVC_Ventas.Models
     {
         //Arrays con los datos de la planilla.
         //Cada arreglo es de 4 items.
-        public int [] code { get; set; } = new int[4];
-        public string [] role { get; set; } = new string [4];
-        public string [] name { get; set; } = new string[4];
-        public int [] salary { get; set; } = new int [4];
+        public int [] code { get; set; } = new int[5];
+        public string [] role { get; set; } = new string [5];
+        public string [] name { get; set; } = new string[5];
+        public int [] salary { get; set; } = new int [5];
+        public List<double> sales { get; set; }
 
+        //Metodos para el calculo de los datos
+        public double semanal(double venta)
+        {
+            double montoSemanal = venta * 0.23;
+            return Math.Round(montoSemanal,2);
+        }
+        public double comision(double venta)
+        {
+            if (venta < 300000)
+            {
+                return Math.Round(venta * 0.025, 2);
+            }
+            if ((venta >= 300000) && (venta<450000))
+            {
+                return Math.Round(venta * 0.05, 2);
+            }
+            if ((venta >= 450000) && (venta<600000))
+            {
+                return Math.Round(venta * 0.075, 2);
+            }
+            if (venta >= 600000)
+            {
+                return Math.Round(venta * 0.1, 2);
+            }
+            else
+                return venta;
+        }
+        public double deduccion(double salario, double comision) {
+            double montoDeduccion = Math.Round((salario + comision) * 0.09);
+            return montoDeduccion;
+        }
+        public double neto(double salario, double comision, double deduccion)
+        {
+            double montoNeto = Math.Round((salario + comision + deduccion),2);
+            return montoNeto;
+        }
     }
 }
