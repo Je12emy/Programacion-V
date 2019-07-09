0ed2a0ada8c45f52e4df1b20f5f0923faa033a60
Jeremy <jeremyzelaya@hotmail.es>
2019-07-08 20:59:07 -0600

    Added Semana 8

---
+++ Semana 8/CapaDatos/Capa de Datos/CapaDatos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Capa_de_Datos
+{
+    public class CapaDatos
+    {
+        static string Cadena_Conexion = "Data Source=.;Initial Catalog=MusicStore;Integrated Security=True";
+        static SqlConnection Conexion = new SqlConnection(Cadena_Conexion);
+        public DataTable Ejecutar_Consulta(StringBuilder Query, SqlCommand Comando) {
+            // Es mejor usar un DataTable cuando trabajemos con consultas a una sola tabla.
+            DataTable Tabla = new DataTable();
+            try
+            {
+                Conexion.Open();
+                if (Comando == null)
+                {
+                    Comando = new SqlCommand();
+                }
+                Comando.Connection = Conexion;
+                Comando.CommandType = CommandType.Text;
+                Comando.CommandText = Query.ToString();
+
+                SqlDataReader Lector = Comando.ExecuteReader();
+                // DataReader para tabajar DataTable!
+
+                Tabla.Load(Lector);
+                Conexion.Close();
+
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                Conexion.Close();
+                throw new Exception("Error en Capa de Datos: " + ex);
+                // En el mundo real mostrar el mensaje de error no es lo optimo, pues puede ser informacion sensible.
+            }
+        }
+    }
+}
---
+++ Semana 8/MusicStore/MusicStore/Models/DatosGenre.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace MusicStore.Models
+{
+    public class DatosGenre
+    {
+        CapaDatos _CadaDatos = new CapaDatos();
+        // Creamos una instancia de CapaDatos
+        readonly string mensajeError = "Error en datos genero";
+
+        public DataTable ConsultarGenero(int idGenero = 0) {
+            // Aca declaramos el Parametro de idGenero como 0, pues asi sera Opcional.
+            StringBuilder sqlQuery = new StringBuilder();
+            SqlCommand Comando = new SqlCommand();
+            DataTable Tabla = new DataTable();
+
+            try
+            {
+                // Hacemos un query para seleccionar todos los datos en Genre
+                sqlQuery.Append("SELECT * FROM Genre ");
+                // Al hacer el siguiente podemos especificar el filtro en caso de que nos den un valor
+                if (idGenero != 0)
+                {
+                    sqlQuery.Append(" Where GenreId = @IdGenero");
+                    Comando.Parameters.Add("@IdGenero", SqlDbType.Int).Value = idGenero;
+                    Tabla = _CadaDatos.Ejecutar_Consulta(sqlQuery, Comando);
+                }
+                else {
+                    Tabla = _CadaDatos.Ejecutar_Consulta(sqlQuery);
+                }
+                return Tabla;
+            }
+            catch (Exception)
+            {
+                throw new Exception(mensajeError);
+            }
+        }
+
+
+        public void InsertarGenero(string Nombre,string Descripcion) {
+            StringBuilder sqlQuery = new StringBuilder();
+            SqlCommand Comando = new SqlCommand();
+            int Resultado = 0;
+
+            try
+            {
+                sqlQuery.Append("INSERT INTO Genre VALUES(@Nombre, @Descripcion)");
+                Comando.Parameters.Add("@Nombre", SqlDbType.NVarChar).Value = Nombre.Trim() ;
+                Comando.Parameters.Add("@Descripcion", SqlDbType.NVarChar).Value = Descripcion.Trim() ;
+
+                Resultado = _CadaDatos.EjecutarSentencia(sqlQuery, Comando);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(mensajeError + ex.Message);
+            }
+        }
+        public void ModificarGenero(string IdGenero, string Nombre, string Descripcion) {
+            StringBuilder sqlQuery = new StringBuilder();
+            SqlCommand Comando = new SqlCommand();
+            int Resultado = 0;
+
+            try
+            {
+                sqlQuery.Append("UPDATE Genre SET Name = @Nombre, Description = @Description WHERE GenreId = @IdGenero");
+                Comando.Parameters.Add("@IdGenero", SqlDbType.Int).Value = IdGenero;
+                Comando.Parameters.Add("@Nombre", SqlDbType.NVarChar).Value = Nombre;
+                Comando.Parameters.Add("@Description", SqlDbType.NVarChar).Value = Descripcion;
+
+                Resultado = _CadaDatos.EjecutarSentencia(sqlQuery, Comando);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(mensajeError + ex.Message);
+            }
+        }
+    }
+}
