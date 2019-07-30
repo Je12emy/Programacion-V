66d367d673c610f790d1de1253c71b0bdd6a6eaf
Jeremy <jeremyzelaya@hotmail.es>
2019-07-29 18:56:43 -0600

    Added Contador App

---
+++ Semana 11/ContadorApp/ContadorApp/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ContadorApp
+{
+    [Activity(Label = "ContadorApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int contador = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            TextView et = FindViewById<TextView>(Resource.Id.Contador);
+            Button btn = FindViewById<Button>(Resource.Id.btnContador);
+            
+            btn.Click += delegate
+            {
+                et.Text = "Clicks" + contador.ToString();
+                contador++;
+            };
+
+        }
+    }
+}
+
